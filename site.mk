944ee51add417d50f8b85b3d1e78381dbd3a829a
Simon Müller <simon.f.mueller@gmail.com>
2019-02-19 10:47:40 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -5,119 +5,119 @@
 #		The gluon-mesh-batman-adv-* package must come first because of the dependency resolution
 
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-15 \
-	gluon-respondd \
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
+        gluon-mesh-batman-adv-15 \
+        gluon-respondd \
+        gluon-autoupdater \
+        gluon-config-mode-autoupdater \
+        gluon-config-mode-contact-info \
+        gluon-config-mode-geo-location \
+        gluon-config-mode-hostname \
+        gluon-config-mode-core \
 	gluon-config-mode-mesh-vpn \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-source-filter \
+        gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	respondd-module-airtime \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-wifi-config \
-	gluon-web-network \
+        gluon-radv-filterd \
+        respondd-module-airtime \
+        gluon-web-admin \
+        gluon-web-autoupdater \
+        gluon-web-wifi-config \
+        gluon-web-network \
+        gluon-status-page \
+        gluon-web-private-wifi \
+	gluon-setup-mode \
 	gluon-mesh-vpn-tunneldigger \
 	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
 	haveged \
-	iptables \
-	iwinfo \
-	gluon-web-private-wifi 
-	#gluon-tunneldigger-watchdog
+        iptables \
+        iwinfo
 
+#	gluon-ebtables-source-filter \
 
 # basic support for USB stack
 USB_PACKAGES_BASIC := \
-	kmod-usb-core \
-	kmod-usb2
+        kmod-usb-core \
+        kmod-usb2
 
 # storage support for USB devices
 USB_PACKAGES_STORAGE := \
-	block-mount \
-	blkid \
-	kmod-fs-ext4 \
-	kmod-fs-vfat \
-	kmod-usb-storage \
-	kmod-usb-storage-extras \
-	kmod-nls-cp1250 \
-	kmod-nls-cp1251 \
-	kmod-nls-cp437 \
-	kmod-nls-cp775 \
-	kmod-nls-cp850 \
-	kmod-nls-cp852 \
-	kmod-nls-cp866 \
-	kmod-nls-iso8859-1 \
-	kmod-nls-iso8859-13 \
-	kmod-nls-iso8859-15 \
-	kmod-nls-iso8859-2 \
-	kmod-nls-koi8r \
-	kmod-nls-utf8 \
-	swap-utils
+        block-mount \
+        blkid \
+        kmod-fs-ext4 \
+        kmod-fs-vfat \
+        kmod-usb-storage \
+        kmod-usb-storage-extras \
+        kmod-nls-cp1250 \
+        kmod-nls-cp1251 \
+        kmod-nls-cp437 \
+        kmod-nls-cp775 \
+        kmod-nls-cp850 \
+        kmod-nls-cp852 \
+        kmod-nls-cp866 \
+        kmod-nls-iso8859-1 \
+        kmod-nls-iso8859-13 \
+        kmod-nls-iso8859-15 \
+        kmod-nls-iso8859-2 \
+        kmod-nls-koi8r \
+        kmod-nls-utf8 \
+        swap-utils
 
 # network support for USB devices
 USB_PACKAGES_NET := \
-	kmod-mii \
-	kmod-nls-base \
-	kmod-usb-net \
-	kmod-usb-net-asix \
-	kmod-usb-net-asix-ax88179 \
-	kmod-usb-net-cdc-eem \
-	kmod-usb-net-cdc-ether \
-	kmod-usb-net-cdc-mbim \
-	kmod-usb-net-cdc-ncm \
-	kmod-usb-net-cdc-subset \
-	kmod-usb-net-dm9601-ether \
-	kmod-usb-net-hso \
-	kmod-usb-net-huawei-cdc-ncm \
-	kmod-usb-net-ipheth \
-	kmod-usb-net-kalmia \
-	kmod-usb-net-kaweth \
-	kmod-usb-net-mcs7830 \
-	kmod-usb-net-pegasus \
-	kmod-usb-net-qmi-wwan \
-	kmod-usb-net-rndis \
-	kmod-usb-net-sierrawireless \
-	kmod-usb-net-smsc95xx
+        kmod-mii \
+        kmod-nls-base \
+        kmod-usb-net \
+        kmod-usb-net-asix \
+        kmod-usb-net-asix-ax88179 \
+        kmod-usb-net-cdc-eem \
+        kmod-usb-net-cdc-ether \
+        kmod-usb-net-cdc-mbim \
+        kmod-usb-net-cdc-ncm \
+        kmod-usb-net-cdc-subset \
+        kmod-usb-net-dm9601-ether \
+        kmod-usb-net-hso \
+        kmod-usb-net-huawei-cdc-ncm \
+        kmod-usb-net-ipheth \
+        kmod-usb-net-kalmia \
+        kmod-usb-net-kaweth \
+        kmod-usb-net-mcs7830 \
+        kmod-usb-net-pegasus \
+        kmod-usb-net-qmi-wwan \
+        kmod-usb-net-rndis \
+        kmod-usb-net-sierrawireless \
+        kmod-usb-net-smsc95xx
 # broken
-#	kmod-usb-net-rtl8150 \
-#	kmod-usb-net-rtl8152 \
+#       kmod-usb-net-rtl8150 \
+#       kmod-usb-net-rtl8152 \
 # network support for PCI devices
 PCI_PACKAGES_NET := \
-	kmod-3c59x \
-	kmod-e100 \
-	kmod-e1000 \
-	kmod-e1000e \
-	kmod-forcedeth \
-	kmod-natsemi \
-	kmod-ne2k-pci \
-	kmod-pcnet32 \
-	kmod-r8169 \
-	kmod-sis900 \
-	kmod-sky2 \
-	kmod-tg3 \
-	kmod-tulip \
-	kmod-via-rhine \
+        kmod-3c59x \
+        kmod-e100 \
+        kmod-e1000 \
+        kmod-e1000e \
+        kmod-forcedeth \
+        kmod-natsemi \
+        kmod-ne2k-pci \
+        kmod-pcnet32 \
+        kmod-r8169 \
+        kmod-sis900 \
+        kmod-sky2 \
+        kmod-tg3 \
+        kmod-tulip \
+        kmod-via-rhine \
         kmod-8139cp
-
 # broken
-#	kmod-ixgbe \
-#	kmod-r8139too \
+#       kmod-ixgbe \
+#       kmod-r8139too \
 # additional packages
 TOOLS_PACKAGES := \
-	iperf \
-	socat \
-	tcpdump \
-	usbutils \
-	vnstat
+        iperf \
+        socat \
+        tcpdump \
+        usbutils \
+        vnstat
 # broken
-#	pciutils \
+#       pciutils \
+#
 #
 # $(GLUON_TARGET) specific settings:
 #
@@ -127,13 +127,13 @@ ifeq ($(GLUON_TARGET),x86-generic)
 # support the usb stack on x86 devices
 # and add a few common USB and PCI NICs
 GLUON_SITE_PACKAGES += \
-	kmod-usb-hid \
-	kmod-hid-generic \
-	$(USB_PACKAGES_BASIC) \
-	$(USB_PACKAGES_STORAGE) \
-	$(USB_PACKAGES_NET) \
-	$(PCI_PACKAGES_NET) \
-	$(TOOLS_PACKAGES)
+        kmod-usb-hid \
+        kmod-hid-generic \
+        $(USB_PACKAGES_BASIC) \
+        $(USB_PACKAGES_STORAGE) \
+        $(USB_PACKAGES_NET) \
+        $(PCI_PACKAGES_NET) \
+        $(TOOLS_PACKAGES)
 endif
 
 # x86-64
@@ -141,34 +141,44 @@ ifeq ($(GLUON_TARGET),x86-64)
 # support the usb stack on x86-64 devices
 # and add a few common USB and PCI NICs
 GLUON_SITE_PACKAGES += \
-	kmod-usb-hid \
-	kmod-hid-generic \
-	$(USB_PACKAGES_BASIC) \
-	$(USB_PACKAGES_STORAGE) \
-	$(USB_PACKAGES_NET) \
-	$(PCI_PACKAGES_NET) \
-	$(TOOLS_PACKAGES)
+        kmod-usb-hid \
+        kmod-hid-generic \
+        $(USB_PACKAGES_BASIC) \
+        $(USB_PACKAGES_STORAGE) \
+        $(USB_PACKAGES_NET) \
+        $(PCI_PACKAGES_NET) \
+        $(TOOLS_PACKAGES)
 endif
-
 ifeq ($(GLUON_TARGET),x86-kvm_guest)
 GLUON_SITE_PACKAGES += \
-	$(TOOLS_PACKAGES)
+        $(TOOLS_PACKAGES)
 endif
 
-# ar71xx-generic
-GLUON_TLWR842_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_TLWR1043_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_TLWR2543_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_TLWDR4300_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_WNDR3700_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_WRT160NL_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_ARCHERC7_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_GLINET_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_WZRHPG450H_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
-GLUON_WZRHPAG300H_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+#ar71xx-generic
+GLUON_tp-link-tl-wr842n-nd-v3_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_tp-link-tl-wr1043n-nd-v2_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_tp-link-tl-wr1043n-nd-v3_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_tp-link-tl-wr1043n-nd-v4_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_tp-link-tl-wr1043n-v5_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_tp-link-tl-wdr4300-v1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_tp-link-tl-wr2543n-nd-v1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_linksys-wrt160nl_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_d-link-dir-825-rev-b1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_d-link-dir-505-rev-a1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_d-link-dir-505-rev-a2_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_gl-inet-6408a-v1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_gl-inet-6416a-v1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_netgear-wndr3700_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_netgear-wndr3700v2_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_netgear-wndr3700v4_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_buffalo-wzr-hp-g450h_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_buffalo-wzr-hp-g300nh_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_tp-link-archer-c7-v2_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+GLUON_tp-link-archer-c7-v4_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
 
-# mpc85xx-generic
-GLUON_TLWDR4900_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+#mpc85xx-generic
+GLUON_tp-link-tl-wdr4900-v1_SITE_PACKAGES := $(USB_PACKAGES_BASIC) $(TOOLS_PACKAGES) $(USB_PACKAGES_STORAGE)
+
 
 ##	DEFAULT_GLUON_RELEASE
 #		version string to use for images
